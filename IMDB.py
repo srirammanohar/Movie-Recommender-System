7d385d1a7dbd3deada2051346b854045b0dc1b61
Abhishek Majumdar <rudra9335@gmail.com>
2015-11-24 22:34:36 -0800

    Extract json with omdb api
    Signed-off-by: Abhishek Majumdar <rudra9335@gmail.com>

--- IMDB.py
+++ IMDB.py
@@ -15,15 +15,15 @@ for l in a:
     if l['imdbId'] not in set:
         set.append(l['imdbId'])
 i=0
+print len(set)
 for i in range(0,len(set)):
-    print i
     j="http://www.omdbapi.com/?i=tt"+str(set[i])+"&plot=short&r=json"
     try:
         req = urllib2.Request(j)
         response = urllib2.urlopen(req)
         the_page = response.read()
         data.append(the_page)
-    except urllib2.HTTPError as e:
+    except:
         print i, "whoops error!"
         continue
 print len(data)
