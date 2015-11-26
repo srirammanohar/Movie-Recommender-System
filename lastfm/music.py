ab69beca671fee5617b4e5642ee172e5efe52fff
Aravind Kumar K <akumark@eng.ucsd.edu>
2015-11-26 10:34:31 -0800

    fixing json dumping error
    and in the format required

--- lastfm/music.py
+++ lastfm/music.py
@@ -6,7 +6,7 @@ import json
 
 network = pylast.LastFMNetwork(api_key = single_init.API_KEY, api_secret =
     single_init.API_SECRET, username = single_init.username, password_hash = single_init.password_hash)
-user =  pylast.Library('RJ',network)
+user =  pylast.Library('joi',network)
 friends =[]
 print type(user)
 print user
@@ -17,7 +17,7 @@ user_list.append(user.get_user())
 #a = user.get_recent_tracks()
 print type(network)
 #a = [str(a[i]) for i in range(len(a))]
-top_artists=collections.defaultdict(list)
+top_artists = collections.defaultdict(lambda : collections.defaultdict(list))
 # temp=user.get_artists(50,True)
 # for i in range(len(temp)):
 # #user.get_artists(50,True)[0].playcount
@@ -49,19 +49,21 @@ def create_user_data(userlist, layer ):
             swaplist = swaplist + i.get_friends()
             newuser =  pylast.Library(i,network)
             templ = newuser.get_artists(50,True)
-            if len(top_artists[newuser.get_user()]) == 0:
+            if len(top_artists[str(newuser.get_user())][str(newuser.get_user())]) == 0:
                 for j in range(len(templ)):
-                    top_artists[newuser.get_user()].append([str(templ[j].item),templ[j].playcount])
+                    top_artists[str(newuser.get_user())][str(newuser.get_user())].append([str(templ[j].item),templ[j].playcount])
         create_user_data(swaplist,layer)
     except IndexError:
         print i
 
-
-create_user_data(user_list,4)
+create_user_data(user_list,2)
+print top_artists['joi']
 with open('user.json', 'w') as fp:
-    json.dump(top_artists, fp)
-
+    for a in top_artists:
+    	json.dump(top_artists[a], fp)
+    	fp.write("\n")
 print len(top_artists)
+
 # count = 0
 # for i in top_artists:
 #     print top_artists[i]
